52890f3cd102711d3f86b16d087abab711404165
nvrstyle <nvrstyle@gmail.com>
2019-11-03 13:31:08 +0300

    Rename basic src package

--- src/main/java/ru/lubich/talkushka/GreetingController.java
+++ src/main/java/ru/lubich/talkushka/GreetingController.java
@@ -1,8 +1,6 @@
-package ru.lubich.sweater;
+package ru.lubich.talkushka;
 
-import com.sun.javafx.collections.MappingChange;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
